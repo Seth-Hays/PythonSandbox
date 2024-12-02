802f30d6dff1759b5aad521897cec5462c7fcb36
I Run With Scissors <147119210+Seth-Hays@users.noreply.github.com>
2024-12-02 22:54:11 +0000

    added functions

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 """
-Type your header comment.
+Rolls some stuff.
 Hays - November 2024
 """
 
+import random
+
+def roll_n_times(n:int) -> int:
+    total: int = 0
+    for _ in range (n):
+        roll: int = random.randint(1,6)
+        total += roll
+    return total
+
+def is_winner(total: int, min_to_win: int) -> bool:
+    if total > min_to_win:
+        return True
+    else:
+        return False
 
 def main() -> None:
-    word: str = input()
-    a,b = word.split(" ")
+    #variable setup
+    MIN_WIN: int = 13
+    roll_num: int = 3
 
-    speed: int = int(a)
-    isBday: bool = bool(b)
+  #how many times it rolls the dice
+    total: int = roll_n_times(roll_num)
 
-    if isBday:
-        speed = speed - 5
+    #print out the results
+    print(total)
 
-    if speed <= 60:
-        print("no ticket")
-    
-    elif speed > 80:
-        print("big ticket")
-    
+  #if win
+    if is_winner(total, MIN_WIN):
+        print(f"You won ${total}")
+  #if fail
     else:
-        print("small ticket")
+        print(f"HA HA YOU ONLY GOT {total}! HOW TERRIBLE ARE YOU????")
 
+
+if __name__ == "__main__":
+    main()
